3d1dfba19aaef5783a2568599c26d2d25a59b0d5
BrijeshBumrela <brijeshbumrela@gmail.com>
2020-07-14 18:41:22 +0530

    initial setup

---
+++ public/config.js
@@ -0,0 +1,4 @@
+window.logowlConnectivity = {
+    // Set the URL of the Ray Gun backend without the slash at the end
+    backendURL: 'http://localhost:2800'
+};
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// import Authentication from './components/auth/authentication/Authentication';
+// import ErrorDetails from './components/service/errorDetails/ErrorDetails';
+// import Organization from './components/organization/Organization';
+// import Blueprint from './components/layout/blueprint/Blueprint';
+// import Services from './components/services/Services';
+// import Settings from './components/settings/Settings';
+// import Toast from './components/layout/toast/Toast';
+// import Service from './components/service/Service';
+// import Setup from './components/auth/setup/Setup';
+
+const App = () => (
+    <>
+        {/* <Switch>
+            <Route path='/auth/signin' component={Authentication} />
+            <Route path='/auth/signup' component={Authentication} />
+            <Route path='/auth/setup' component={Setup} />
+            
+            <Blueprint>
+                <Route exact path='/services' component={Services} />
+                <Route exact path='/services/:serviceId' component={Service} />
+                <Route exact path='/services/:serviceId/error/:errorId' component={ErrorDetails} />
+                
+                <Route exact path='/organization' component={Organization} />
+                
+                <Route exact path='/settings' component={Settings} />
+            </Blueprint>
+        </Switch>
+        
+        <Toast /> */}
+        <h1>hi</h1>
+    </>
+);
+
+export default App;
